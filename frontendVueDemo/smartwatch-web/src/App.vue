afd8b20b1302f44f9bd0d6137101fde57aaa7888
Zhenhai Qi <qizhenhai00@outlook.com>
2023-07-07 15:39:33 +0100

    upload Vue framework

---
+++ frontendVueDemo/smartwatch-web/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <IndexComponent></IndexComponent>
+  </div>
+</template>
+
+<script>
+
+
+import IndexComponent from "@/components/IndexComponent.vue";
+
+export default {
+  name: 'App',
+  components: {
+    IndexComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  height: 100%;
+}
+</style>
---
+++ frontendVueDemo/smartwatch-web/src/components/HeaderComponent.vue
@@ -0,0 +1,36 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="header">
+    <div class="container clearfix">
+      <!-- university logo -->
+      <div class="logo leftfix">
+        <div class="logoset">
+          <img src="../assets/UoB-logo.png" alt="UoB-logo">
+        </div>
+      </div>
+      <!-- search area -->
+      <div class="search rightfix">
+        <form action="#">
+          <input type="text">
+          <button></button>
+        </form>
+      </div>
+    </div>
+  </div>
+<!--    <el-dropdown>-->
+<!--      <i class="el-icon-setting" style="margin-right: 15px"></i>-->
+<!--      <el-dropdown-menu slot="dropdown">-->
+<!--        <el-dropdown-item>查看</el-dropdown-item>-->
+<!--        <el-dropdown-item>新增</el-dropdown-item>-->
+<!--        <el-dropdown-item>删除</el-dropdown-item>-->
+<!--      </el-dropdown-menu>-->
+<!--    </el-dropdown>-->
+<!--    <span>王小虎</span>-->
+</template>
+
+<style scoped>
+
+</style>
