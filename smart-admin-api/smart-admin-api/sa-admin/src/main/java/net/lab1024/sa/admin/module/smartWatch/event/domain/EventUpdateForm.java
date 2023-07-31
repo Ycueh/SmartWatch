e5d06e9ca6da7d2274ec2f0da163b0a33d271370
Haozhe Yang <haozheyang382@gmail.com>
2023-07-31 22:55:16 +0100

    update event and dataitem updateform

--- smart-admin-api/smart-admin-api/sa-admin/src/main/java/net/lab1024/sa/admin/module/smartWatch/event/domain/EventUpdateForm.java
+++ smart-admin-api/smart-admin-api/sa-admin/src/main/java/net/lab1024/sa/admin/module/smartWatch/event/domain/EventUpdateForm.java
@@ -2,11 +2,12 @@ package net.lab1024.sa.admin.module.smartWatch.event.domain;
 
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+import net.lab1024.sa.admin.module.smartWatch.response.domain.ResponseAddForm;
 
 import javax.validation.constraints.NotNull;
 
 @Data
-public class EventUpdateForm {
+public class EventUpdateForm extends EventAddForm {
     @ApiModelProperty("id")
     @NotNull(message = "id could not be null")
     private Long id;
