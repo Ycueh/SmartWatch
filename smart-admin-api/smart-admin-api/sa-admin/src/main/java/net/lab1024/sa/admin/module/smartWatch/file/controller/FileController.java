1bc9f71d4418588a0f1311358bfe364f0445bf03
Ycueh <850918164@qq.com>
2023-08-09 22:46:35 +0100

    Reset database

--- smart-admin-api/smart-admin-api/sa-admin/src/main/java/net/lab1024/sa/admin/module/smartWatch/file/controller/FileController.java
+++ smart-admin-api/smart-admin-api/sa-admin/src/main/java/net/lab1024/sa/admin/module/smartWatch/file/controller/FileController.java
@@ -1,14 +1,14 @@
 package net.lab1024.sa.admin.module.smartWatch.file.controller;
 
-import cn.hutool.core.io.resource.InputStreamResource;
+import net.lab1024.sa.admin.module.smartWatch.file.service.FileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
 import io.swagger.annotations.Api;
 import net.lab1024.sa.admin.constant.AdminSwaggerTagConst;
+import net.lab1024.sa.admin.module.smartWatch.response.service.ResponseService;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 
 @RestController
@@ -16,6 +16,8 @@ import java.io.*;
 @Api(tags = {AdminSwaggerTagConst.Business.SW_FILE})
 public class FileController {
     String filePath = "database\\smart_admin_v2.db";
+    @Autowired
+    FileService fileService;
     @GetMapping("/file/download")
     public ResponseEntity<InputStreamResource> downloadFile() {
         File file = new File(filePath);
@@ -28,7 +30,11 @@ public class FileController {
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             }
-
+            try {
+                fileService.resetDatabase();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
             return ResponseEntity.ok()
                     // Content-Disposition
                     .header("Content-Disposition", "attachment;filename=" + filename)
@@ -38,44 +44,6 @@ public class FileController {
                     .contentLength(file.length())
                     .body(resource);
         }
-
         return ResponseEntity.notFound().build();
     }
-//    public void downloadFile( HttpServletResponse response){
-//        File file = new File(filePath);
-//        String filename = file.getName();
-//        BufferedInputStream bis = null;
-//        OutputStream os = null;
-//        try {
-//            //文件是否存在
-//            if (file.exists()) {
-//                //设置响应
-//                response.setContentType("application/octet-stream;charset=UTF-8");
-//                response.setHeader("Access-Control-Expose-Headers", "Content-Disposition");
-//                response.setHeader("Content-Disposition","attachment;filename="+filename);
-//                os = response.getOutputStream();
-//                bis = new BufferedInputStream(new FileInputStream(file));
-//                byte[] buffer = new byte[1024];
-//                int i = bis.read(buffer);
-//                while(i != -1){
-//                    os.write(buffer, 0, i);
-//                    i = bis.read(buffer);
-//                }
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        } finally {
-//            try {
-//                if(bis != null) {
-//                    bis.close();
-//                }
-//                if(os != null) {
-//                    os.flush();
-//                    os.close();
-//                }
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//    }
 }
--- smart-admin-api/smart-admin-api/sa-admin/src/main/java/net/lab1024/sa/admin/module/smartWatch/file/service/FileService.java
+++ smart-admin-api/smart-admin-api/sa-admin/src/main/java/net/lab1024/sa/admin/module/smartWatch/file/service/FileService.java
@@ -1,55 +1,30 @@
 package net.lab1024.sa.admin.module.smartWatch.file.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-import org.springframework.util.FileCopyUtils;
 
+import org.springframework.stereotype.Service;;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-
 
 @Service
 public class FileService {
-    private final ResourceLoader resourceLoader;
+    String sourceFilePath = "database\\resetDatabase\\EMADATA.db";
+    String targetFilePath = "database\\smart_admin_v2.db";
+    public void resetDatabase() throws IOException{
+        File sourceFile = new File(sourceFilePath);
+        File targetFile = new File(targetFilePath);
 
-    @Autowired
-    public FileService(ResourceLoader resourceLoader) {
-        this.resourceLoader = resourceLoader;
-    }
+        if (!targetFile.exists()) {
+            targetFile.createNewFile();
+        }
 
-    public ResponseEntity<byte[]> download() throws IOException {
-        // Construct the relative path to the smart_admin_v2.db file
-        String relativePath = "database\\smart_admin_v2.db";
-//        String relativePath = "database\\1.pdf";
-        // Load the resource using ResourceLoader
-        Resource resource = resourceLoader.getResource("file:" + relativePath);
-
-        // Check if the resource (file) exists
-        if (resource.exists()) {
-            // Load the embedded database file
-            try (InputStream inputStream = resource.getInputStream()) {
-                // Convert the database file to a byte array
-                byte[] databaseBytes = FileCopyUtils.copyToByteArray(inputStream);
-
-                // Set the response headers for download
-                HttpHeaders headers = new HttpHeaders();
-                headers.setContentDispositionFormData("attachment", "exported_smart_admin_v2.db");
-                headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
-
-                // Return the byte array as ResponseEntity for download
-                return new ResponseEntity<>(databaseBytes, headers, HttpStatus.OK);
-            }
-        } else {
-            // Handle the case when the file doesn't exist
-            throw new IOException("Database file not found: " + relativePath);
+        try (FileInputStream fis = new FileInputStream(sourceFile);
+             FileOutputStream fos = new FileOutputStream(targetFile)) {
+            fis.getChannel().transferTo(0, sourceFile.length(), fos.getChannel());
         }
     }
+
 }
 
 
