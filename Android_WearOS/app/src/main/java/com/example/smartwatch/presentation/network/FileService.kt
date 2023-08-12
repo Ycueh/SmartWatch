40ab189d593917b651c29e17a73440af64250ae6
Zhenhai Qi <qizhenhai00@outlook.com>
2023-08-12 16:08:46 +0100

    add token in Android APP

--- Android_WearOS/app/src/main/java/com/example/smartwatch/presentation/network/FileService.kt
+++ Android_WearOS/app/src/main/java/com/example/smartwatch/presentation/network/FileService.kt
@@ -4,6 +4,7 @@ import okhttp3.MultipartBody
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
@@ -11,8 +12,13 @@ import retrofit2.http.Part
 interface FileService {
     @Multipart
     @POST("/api/upload")
-    fun uploadFile(@Part filePart: MultipartBody.Part): Call<Void>
+    fun uploadFile(
+        @Header("Authorization") token: String,
+        @Part filePart: MultipartBody.Part
+    ): Call<Void>
 
     @GET("/file/download")
-    fun downloadFile(): Call<ResponseBody>
+    fun downloadFile(
+        @Header("Authorization") token: String
+    ): Call<ResponseBody>
 }
