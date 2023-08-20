e59cdbfc4fa133093616f302de34606921f3a60f
XDF <1246041011@qq.com>
2023-08-20 19:22:26 +0100

    batchdelete

--- smart-admin-web/javascript-ant-design-vue3/src/views/smartWatch/dataItem/dataItem-list.vue
+++ smart-admin-web/javascript-ant-design-vue3/src/views/smartWatch/dataItem/dataItem-list.vue
@@ -1,184 +1,278 @@
 <template>
+  <!---------- 查询表单form begin ----------->
   <a-form class="smart-query-form">
     <a-row class="smart-query-form-row">
-        <a-form-item label="dataItem" class="smart-query-form-item">
-          <a-input style="width: 200px" v-model:value="queryForm.searchDataItem" placeholder="dataItem" />
-        </a-form-item>
-  
-        <a-form-item class="smart-query-form-item">
-          <a-button type="primary" @click="queryData">
-            <template #icon>
-              <ReloadOutlined />
-            </template>
-            Search
-          </a-button>
-          <a-button @click="resetQuery" class="smart-margin-left10">
-            <template #icon>
-              <SearchOutlined />
-            </template>
-            Reset
-          </a-button>
-        </a-form-item>
-      </a-row>
+      <a-form-item label="datestamp" class="smart-query-form-item">
+        <SmartEnumSelect width="200px" v-model:value="queryForm.datestamp"  placeholder="date" />
+      </a-form-item>
+      <a-form-item label="Keyword" class="smart-query-form-item">
+        <a-input style="width: 200px" v-model:value="queryForm.keyword" placeholder="Keyword" />
+      </a-form-item>
+      <a-form-item label="timestamp" class="smart-query-form-item">
+        <a-input style="width: 200px" v-model:value="queryForm.timestamp" placeholder="timestamp" />
+      </a-form-item>
+      <a-form-item label="dataItem1" class="smart-query-form-item">
+        <a-input style="width: 200px" v-model:value="queryForm.dataItem1" placeholder="dataItem1" />
+      </a-form-item>
+      <a-form-item label="dataItem2" class="smart-query-form-item">
+        <a-input style="width: 200px" v-model:value="queryForm.dataItem2" placeholder="dataItem2" />
+      </a-form-item>
+      <a-form-item label="dataItem3" class="smart-query-form-item">
+        <a-input style="width: 200px" v-model:value="queryForm.dataItem3" placeholder="dataItem3" />
+      </a-form-item>
+      <a-form-item class="smart-query-form-item">
+        <a-button type="primary" @click="queryData">
+          <template #icon>
+            <SearchOutlined />
+          </template>
+          Search
+        </a-button>
+        <a-button @click="resetQuery" class="smart-margin-left10">
+          <template #icon>
+            <ReloadOutlined />
+          </template>
+          Reset
+        </a-button>
+      </a-form-item>
+    </a-row>
   </a-form>
+  <!---------- 查询表单form end ----------->
 
   <a-card size="small" :bordered="false" :hoverable="true">
+    <!---------- 表格操作行 begin ----------->
     <a-row class="smart-table-btn-block">
       <div class="smart-table-operate-block">
-        <a-button @click="addDataItem" type="primary" size="small" v-privilege="'smartWatch:dataItem:add'">
+        <a-button @click="confirmBatchDelete" type="danger" size="small" :disabled="selectedRowKeyList.length == 0" v-privilege="'smartWatch:dataItem:batchDelete'">
           <template #icon>
-            <PlusOutlined />
+            <DeleteOutlined />
           </template>
-          Add dataItem
+          batchDelete
         </a-button>
       </div>
-
     </a-row>
+    <!---------- 表格操作行 end ----------->
 
+    <!---------- 表格 begin ----------->
     <a-table
-        size="small"
-        :dataSource="tableData"
-        :columns="columns"
-        row-key="id"
-        bordered
-        :pagination="false"
+      size="small"
+      :dataSource="tableData"
+      :columns="columns"
+      rowKey="id"
+      bordered
+      :pagination="false"
+      :row-selection="{ selectedRowKeys: selectedRowKeyList, onChange: onSelectChange }"
     >
-        <template #bodyCell="{ record, column}">
-          <template v-if="column.dataIndex === 'action'">
-            <div class="smart-table-operate">
-              <a-button @click="addDataItem(record)" type="link" v-privilege="'smartWatch:dataItem:edit'">Edit</a-button>
-              <a-button @click="deleteDataItem(record)" danger type="link" v-privilege="'smartWatch:dataItem:delete'">Delete</a-button>
-            </div>
-          </template>
+      <template #bodyCell="{ text, record, column }">
+        <template v-if="column.dataIndex === 'action'">
+          <div class="smart-table-operate">
+            <a-button @click="onDelete(record)" danger type="link" v-privilege="'smartWatch:dataItem:delete'">Delete</a-button>
+          </div>
         </template>
+      </template>
     </a-table>
+    <!---------- 表格 end ----------->
 
     <div class="smart-query-table-page">
       <a-pagination
-          show-size-changer
-          show-quick-jumper
-          show-less-items
-          :page-size-options="PAGE_SIZE_OPTIONS"
-          :default-page-size="queryForm.pageSize"
-          v-model:current="queryForm.pageNum"
-          v-model:pageSize="queryForm.pageSize"
-          :total="total"
-          @change="queryData"
-          @show-size-change="queryData"
-          :show-total="(total) => `${total} items in total`"
+        showSizeChanger
+        showQuickJumper
+        show-less-items
+        :pageSizeOptions="PAGE_SIZE_OPTIONS"
+        :defaultPageSize="queryForm.pageSize"
+        v-model:current="queryForm.pageNum"
+        v-model:pageSize="queryForm.pageSize"
+        :total="total"
+        @change="queryData"
+        @showSizeChange="queryData"
+        :show-total="(total) => `${total} in total`"
       />
-
     </div>
-    <dataItemFormModal ref="formModal" @reloadList="queryData" />
+
+    <dataItemForm ref="formRef" @reloadList="queryData" />
+
+    <dataItemModal ref="modalRef" />
   </a-card>
 </template>
-
 <script setup>
-import {onMounted, reactive, ref} from "vue";
-import {dataItemApi} from "/@/api/smartWatch/dataItem/dataItem-api";
-import {smartSentry} from "/@/lib/smart-sentry";
-import {message, Modal} from "ant-design-vue";
-import {SmartLoading} from "/@/components/framework/smart-loading";
-import {PAGE_SIZE_OPTIONS} from "/@/constants/common-const";
-import dataItemFormModal from "./dataItem-form-modal.vue";
+  import { reactive, ref, onMounted } from 'vue';
+  import { message, Modal } from 'ant-design-vue';
+  import { SmartLoading } from '/@/components/framework/smart-loading';
+  import { dataItemApi } from '/@/api/smartWatch/dataItem/dataItem-api';
+  import { PAGE_SIZE_OPTIONS } from '/@/constants/common-const';
+  import { smartSentry } from '/@/lib/smart-sentry';
+  import SmartEnumSelect from '/@/components/framework/smart-enum-select/index.vue';
+  import dataItemForm from './dataItem-form-modal.vue';
+  // ---------------------------- 表格列 ----------------------------
 
-const columns = ref([
-  {
-      title: 'ID',
-      dataIndex: 'id',
-    },
+  const columns = ref([
+
     {
-      title: 'datestamp',
+      title: 'Datestamp',
       dataIndex: 'datestamp',
+      ellipsis: true,
     },
     {
-      title: 'timestamp',
+      title: 'Timestamp',
       dataIndex: 'timestamp',
+      ellipsis: true,
     },
     {
-      title: 'dataitem1',
-      dataIndex: 'dataitem1',
+      title: 'dataItem1',
+      dataIndex: 'dataItem1',
+      ellipsis: true,
     },
     {
-      title: 'dataitem2',
-      dataIndex: 'dataitem2',
-    },{
-      title: 'dataitem3',
-      dataIndex: 'dataitem3',
+      title: 'dataItem2',
+      dataIndex: 'dataItem2',
+      ellipsis: true,
     },
-  {
-    title: 'Action',
-    dataIndex: 'action',
-    fixed: 'right',
-    width: 100,
-  },
-]);
-
-const queryFormState = {
-  searchDataItemId: '',
-  searchDataItem: undefined,
-  pageNum: 1,
-  pageSize: 10,
-};
-
-const queryForm = reactive({ ...queryFormState });
-const tableLoading = ref(false);
-const tableData = ref([]);
-const total = ref(0);
-
-function resetQuery() {
-  let pageSize = queryForm.pageSize;
-  Object.assign(queryForm, queryFormState);
-  queryForm.pageSize = pageSize;
-  queryData();
-}
-
-async function queryData() {
-  tableLoading.value = true;
-  try {
-    let queryResult = await dataItemApi.pageQuery(queryForm);
-    console.log(queryResult);
-    tableData.value = queryResult.data.list;
-    total.value = queryResult.data.total;
-  } catch (e) {
-    smartSentry.captureError(e);
-  } finally {
-    tableLoading.value = false;
-  }
-}
-
-onMounted(queryData);
-
-const formModal = ref();
-
-function addDataItem(dataItemData) {
-  formModal.value.showDrawer(dataItemData);
-}
-
-function deleteDataItem(dataItemData) {
-  Modal.confirm({
-    title: 'Notice',
-    content: 'Are you sure you want to delete [' + dataItemData.id + ']?',
-    okText: 'Delete',
-    okType: 'danger',
-    onOk() {
-      singleDelete(dataItemData);
+    {
+      title: 'dataItem3',
+      dataIndex: 'dataItem3',
+      ellipsis: true,
     },
-    cancelText: 'Cancel',
-    onCancel() {},
-  });
-}
+    {
+      title: 'Action',
+      dataIndex: 'action',
+      fixed: 'right',
+      width: 90,
+    },
+  ]);
+    
 
-async function singleDelete(dataItemData) {
-  try {
-    SmartLoading.show();
-    await dataItemApi.delete(dataItemData.id);
-    message.success('Deleted successfully');
+  // ---------------------------- 查询数据表单和方法 ----------------------------
+
+  const queryFormState = {
+    datestamp:undefined,
+    timestamp:undefined,
+    dataItem1:undefined,
+    dataItem2:undefined,
+    dataItem3:undefined,
+    keyword: undefined, //关键字
+    pageNum: 1,
+    pageSize: 10,
+  };
+  // 查询表单form
+  const queryForm = reactive({ ...queryFormState });
+  // 表格加载loading
+  const tableLoading = ref(false);
+  // 表格数据
+  const tableData = ref([]);
+  // 总数
+  const total = ref(0);
+
+  // 重置查询条件
+  function resetQuery() {
+    let pageSize = queryForm.pageSize;
+    Object.assign(queryForm, queryFormState);
+    queryForm.pageSize = pageSize;
     queryData();
-  } catch (e) {
-    smartSentry.captureError(e);
-  } finally {
-    SmartLoading.hide();
   }
-}
+
+  // 查询数据
+  async function queryData() {
+    tableLoading.value = true;
+    try {
+      let queryResult = await dataItemApi.queryPage(queryForm);
+      console.log(queryResult);
+      tableData.value = queryResult.data.list;
+      total.value = queryResult.data.total;
+    } catch (e) {
+      smartSentry.captureError(e);
+    } finally {
+      tableLoading.value = false;
+    }
+  }
+
+  function onChangePublicDate(dates, dateStrings) {
+    queryForm.publicDateBegin = dateStrings[0];
+    queryForm.publicDateEnd = dateStrings[1];
+  }
+
+  onMounted(queryData);
+
+  // ---------------------------- 查看 ----------------------------
+  const modalRef = ref();
+
+  function showModal(data) {
+    modalRef.value.show(data);
+  }
+
+  // ---------------------------- 添加/修改 ----------------------------
+  const formRef = ref();
+
+  function showForm(data) {
+    formRef.value.show(data);
+  }
+
+  // ---------------------------- 单个删除 ----------------------------
+  //确认删除
+  function onDelete(data) {
+    Modal.confirm({
+      title: 'Hint',
+      content: 'Are you sure to delete item [' + data.id + ']?',
+      okText: 'Delete',
+      okType: 'danger',
+      onOk() {
+        requestDelete(data);
+      },
+      cancelText: 'Cancel',
+      onCancel() {},
+    });
+  }
+
+  //请求删除
+  async function requestDelete(data) {
+    SmartLoading.show();
+    try {
+      let deleteForm = {
+        goodsIdList: selectedRowKeyList.value,
+      };
+      await dataItemApi.delete(data.id);
+      message.success('Delete successfully');
+      queryData();
+    } catch (e) {
+      smartSentry.captureError(e);
+    } finally {
+      SmartLoading.hide();
+    }
+  }
+
+  // ---------------------------- 批量删除 ----------------------------
+
+  // 选择表格行
+  const selectedRowKeyList = ref([]);
+
+  function onSelectChange(selectedRowKeys) {
+    selectedRowKeyList.value = selectedRowKeys;
+  }
+
+  // 批量删除
+  function confirmBatchDelete() {
+    Modal.confirm({
+      title: 'Hint',
+      content: 'Are you sure to delete these data',
+      okText: 'Delete',
+      okType: 'danger',
+      onOk() {
+        requestBatchDelete();
+      },
+      cancelText: 'Cancel',
+      onCancel() {},
+    });
+  }
+
+  //请求批量删除
+  async function requestBatchDelete() {
+    try {
+      SmartLoading.show();
+      await dataItemApi.batchDelete(selectedRowKeyList.value);
+      message.success('Delete successfully');
+      queryData();
+    } catch (e) {
+      smartSentry.captureError(e);
+    } finally {
+      SmartLoading.hide();
+    }
+  }
 </script>
--- smart-admin-web/javascript-ant-design-vue3/src/views/smartWatch/event/event-list.vue
+++ smart-admin-web/javascript-ant-design-vue3/src/views/smartWatch/event/event-list.vue
@@ -1,178 +1,260 @@
 <template>
+  <!---------- 查询表单form begin ----------->
   <a-form class="smart-query-form">
     <a-row class="smart-query-form-row">
-
-      <a-form-item label="event" class="smart-query-form-item">
-        <a-input style="width: 200px" v-model:value="queryForm.searchEvent" placeholder="event" />
+      <a-form-item label="eventdate" class="smart-query-form-item">
+        <SmartEnumSelect width="200px" v-model:value="queryForm.eventdate"  placeholder="eventdate" />
       </a-form-item>
-
+      <a-form-item label="Keyword" class="smart-query-form-item">
+        <a-input style="width: 200px" v-model:value="queryForm.keyword" placeholder="Keyword" />
+      </a-form-item>
+      <a-form-item label="eventtime" class="smart-query-form-item">
+        <a-input style="width: 200px" v-model:value="queryForm.eventtime" placeholder="eventtime" />
+      </a-form-item>
+      <a-form-item label="eventdesc" class="smart-query-form-item">
+        <a-input style="width: 200px" v-model:value="queryForm.eventdesc" placeholder="eventdesc" />
+      </a-form-item>
       <a-form-item class="smart-query-form-item">
         <a-button type="primary" @click="queryData">
           <template #icon>
-            <ReloadOutlined />
+            <SearchOutlined />
           </template>
           Search
         </a-button>
         <a-button @click="resetQuery" class="smart-margin-left10">
           <template #icon>
-            <SearchOutlined />
+            <ReloadOutlined />
           </template>
           Reset
         </a-button>
       </a-form-item>
     </a-row>
   </a-form>
+  <!---------- 查询表单form end ----------->
 
   <a-card size="small" :bordered="false" :hoverable="true">
+    <!---------- 表格操作行 begin ----------->
     <a-row class="smart-table-btn-block">
       <div class="smart-table-operate-block">
-        <a-button @click="addevent" type="primary" size="small" v-privilege="'smartWatch:event:add'">
+        <a-button @click="confirmBatchDelete" type="danger" size="small" :disabled="selectedRowKeyList.length == 0" v-privilege="'smartWatch:event:batchDelete'">
           <template #icon>
-            <PlusOutlined />
+            <DeleteOutlined />
           </template>
-          Add event
+          batchDelete
         </a-button>
       </div>
-
     </a-row>
+    <!---------- 表格操作行 end ----------->
 
+    <!---------- 表格 begin ----------->
     <a-table
-        size="small"
-        :dataSource="tableData"
-        :columns="columns"
-        row-key="id"
-        bordered
-        :pagination="false"
+      size="small"
+      :dataSource="tableData"
+      :columns="columns"
+      rowKey="id"
+      bordered
+      :pagination="false"
+      :row-selection="{ selectedRowKeys: selectedRowKeyList, onChange: onSelectChange }"
     >
-      <template #bodyCell="{ record, column}">
+      <template #bodyCell="{ text, record, column }">
         <template v-if="column.dataIndex === 'action'">
           <div class="smart-table-operate">
-            <a-button @click="addevent(record)" type="link" v-privilege="'smartWatch:event:edit'">Edit</a-button>
-            <a-button @click="deleteevent(record)" danger type="link" v-privilege="'smartWatch:event:delete'">Delete</a-button>
+            <a-button @click="onDelete(record)" danger type="link" v-privilege="'smartWatch:event:delete'">Delete</a-button>
           </div>
         </template>
       </template>
     </a-table>
+    <!---------- 表格 end ----------->
 
     <div class="smart-query-table-page">
       <a-pagination
-          show-size-changer
-          show-quick-jumper
-          show-less-items
-          :page-size-options="PAGE_SIZE_OPTIONS"
-          :default-page-size="queryForm.pageSize"
-          v-model:current="queryForm.pageNum"
-          v-model:pageSize="queryForm.pageSize"
-          :total="total"
-          @change="queryData"
-          @show-size-change="queryData"
-          :show-total="(total) => `${total} items in total`"
+        showSizeChanger
+        showQuickJumper
+        show-less-items
+        :pageSizeOptions="PAGE_SIZE_OPTIONS"
+        :defaultPageSize="queryForm.pageSize"
+        v-model:current="queryForm.pageNum"
+        v-model:pageSize="queryForm.pageSize"
+        :total="total"
+        @change="queryData"
+        @showSizeChange="queryData"
+        :show-total="(total) => `${total} in total`"
       />
-
     </div>
-    <eventFormModal ref="formModal" @reloadList="queryData" />
+
+    <eventForm ref="formRef" @reloadList="queryData" />
+
+    <eventModal ref="modalRef" />
   </a-card>
 </template>
-
 <script setup>
-import {onMounted, reactive, ref} from "vue";
-import {eventApi} from "/@/api/smartWatch/event/event-api";
-import {smartSentry} from "/@/lib/smart-sentry";
-import {message, Modal} from "ant-design-vue";
-import {SmartLoading} from "/@/components/framework/smart-loading";
-import {PAGE_SIZE_OPTIONS} from "/@/constants/common-const";
-import eventFormModal from "./event-form-modal.vue";
+  import { reactive, ref, onMounted } from 'vue';
+  import { message, Modal } from 'ant-design-vue';
+  import { SmartLoading } from '/@/components/framework/smart-loading';
+  import { eventApi } from '/@/api/smartWatch/event/event-api';
+  import { PAGE_SIZE_OPTIONS } from '/@/constants/common-const';
+  import { smartSentry } from '/@/lib/smart-sentry';
+  import SmartEnumSelect from '/@/components/framework/smart-enum-select/index.vue';
+  import eventForm from './event-form-modal.vue';
+  // ---------------------------- 表格列 ----------------------------
 
-const columns = ref([
-  {
-    title: 'ID',
-    dataIndex: 'id',
-  },
-  {
-    title: 'eventdate',
-    dataIndex: 'eventdate',
-  },
-  {
-    title: 'eventtime',
-    dataIndex: 'eventtime',
-  },
-  {
-    title: 'eventdesc',
-    dataIndex: 'eventdesc',
-  },
- 
-  {
-    title: 'Action',
-    dataIndex: 'action',
-    fixed: 'right',
-    width: 100,
-  },
-]);
+  const columns = ref([
 
-const queryFormState = {
-  searchEventId: '',
-  searchEvent: undefined,
-  pageNum: 1,
-  pageSize: 10,
-};
-
-const queryForm = reactive({ ...queryFormState });
-const tableLoading = ref(false);
-const tableData = ref([]);
-const total = ref(0);
-
-function resetQuery() {
-  let pageSize = queryForm.pageSize;
-  Object.assign(queryForm, queryFormState);
-  queryForm.pageSize = pageSize;
-  queryData();
-}
-
-async function queryData() {
-  tableLoading.value = true;
-  try {
-    let queryResult = await eventApi.pageQuery(queryForm);
-    tableData.value = queryResult.data.list;
-    total.value = queryResult.data.total;
-  } catch (e) {
-    smartSentry.captureError(e);
-  } finally {
-    tableLoading.value = false;
-  }
-}
-
-onMounted(queryData);
-
-const formModal = ref();
-
-function addevent(eventData) {
-  formModal.value.showDrawer(eventData);
-}
-
-function deleteevent(eventData) {
-  Modal.confirm({
-    title: 'Notice',
-    content: 'Are you sure you want to delete [' + eventData.id + ']?',
-    okText: 'Delete',
-    okType: 'danger',
-    onOk() {
-      singleDelete(eventData);
+    {
+      title: 'Eventdate',
+      dataIndex: 'eventdate',
+      ellipsis: true,
     },
-    cancelText: 'Cancel',
-    onCancel() {},
-  });
-}
+    {
+      title: 'Eventtime',
+      dataIndex: 'eventtime',
+      ellipsis: true,
+    },
+    {
+      title: 'Eventdesc',
+      dataIndex: 'eventdesc',
+      ellipsis: true,
+    },
+    {
+      title: 'Action',
+      dataIndex: 'action',
+      fixed: 'right',
+      width: 90,
+    },
+  ]);
+    
 
-async function singleDelete(eventData) {
-  try {
-    SmartLoading.show();
-    await eventApi.delete(eventData.id);
-    message.success('Deleted successfully');
+  // ---------------------------- 查询数据表单和方法 ----------------------------
+
+  const queryFormState = {
+    eventdate:undefined,
+    eventtime:undefined,
+    eventdesc:undefined,
+    keyword: undefined, //关键字
+    pageNum: 1,
+    pageSize: 10,
+  };
+  // 查询表单form
+  const queryForm = reactive({ ...queryFormState });
+  // 表格加载loading
+  const tableLoading = ref(false);
+  // 表格数据
+  const tableData = ref([]);
+  // 总数
+  const total = ref(0);
+
+  // 重置查询条件
+  function resetQuery() {
+    let pageSize = queryForm.pageSize;
+    Object.assign(queryForm, queryFormState);
+    queryForm.pageSize = pageSize;
     queryData();
-  } catch (e) {
-    smartSentry.captureError(e);
-  } finally {
-    SmartLoading.hide();
   }
-}
+
+  // 查询数据
+  async function queryData() {
+    tableLoading.value = true;
+    try {
+      let queryResult = await eventApi.queryPage(queryForm);
+      console.log(queryResult);
+      tableData.value = queryResult.data.list;
+      total.value = queryResult.data.total;
+    } catch (e) {
+      smartSentry.captureError(e);
+    } finally {
+      tableLoading.value = false;
+    }
+  }
+
+  function onChangePublicDate(dates, dateStrings) {
+    queryForm.publicDateBegin = dateStrings[0];
+    queryForm.publicDateEnd = dateStrings[1];
+  }
+
+  onMounted(queryData);
+
+  // ---------------------------- 查看 ----------------------------
+  const modalRef = ref();
+
+  function showModal(data) {
+    modalRef.value.show(data);
+  }
+
+  // ---------------------------- 添加/修改 ----------------------------
+  const formRef = ref();
+
+  function showForm(data) {
+    formRef.value.show(data);
+  }
+
+  // ---------------------------- 单个删除 ----------------------------
+  //确认删除
+  function onDelete(data) {
+    Modal.confirm({
+      title: 'Hint',
+      content: 'Are you sure to delete item [' + data.id + ']?',
+      okText: 'Delete',
+      okType: 'danger',
+      onOk() {
+        requestDelete(data);
+      },
+      cancelText: 'Cancel',
+      onCancel() {},
+    });
+  }
+
+  //请求删除
+  async function requestDelete(data) {
+    SmartLoading.show();
+    try {
+      let deleteForm = {
+        goodsIdList: selectedRowKeyList.value,
+      };
+      await eventApi.delete(data.id);
+      message.success('Delete successfully');
+      queryData();
+    } catch (e) {
+      smartSentry.captureError(e);
+    } finally {
+      SmartLoading.hide();
+    }
+  }
+
+  // ---------------------------- 批量删除 ----------------------------
+
+  // 选择表格行
+  const selectedRowKeyList = ref([]);
+
+  function onSelectChange(selectedRowKeys) {
+    selectedRowKeyList.value = selectedRowKeys;
+  }
+
+  // 批量删除
+  function confirmBatchDelete() {
+    Modal.confirm({
+      title: 'Hint',
+      content: 'Are you sure to delete these data',
+      okText: 'Delete',
+      okType: 'danger',
+      onOk() {
+        requestBatchDelete();
+      },
+      cancelText: 'Cancel',
+      onCancel() {},
+    });
+  }
+
+  //请求批量删除
+  async function requestBatchDelete() {
+    try {
+      SmartLoading.show();
+      await eventApi.batchDelete(selectedRowKeyList.value);
+      message.success('Delete successfully');
+      queryData();
+    } catch (e) {
+      smartSentry.captureError(e);
+    } finally {
+      SmartLoading.hide();
+    }
+  }
 </script>
