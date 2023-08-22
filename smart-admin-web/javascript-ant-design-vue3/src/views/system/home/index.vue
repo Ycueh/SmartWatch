5244b9cfffd4842b2143599084bd9cb490c27c42
Zhenhai Qi <qizhenhai00@outlook.com>
2023-08-22 22:21:36 +0100

    update home page in front end

--- smart-admin-web/javascript-ant-design-vue3/src/views/system/home/index.vue
+++ smart-admin-web/javascript-ant-design-vue3/src/views/system/home/index.vue
@@ -9,6 +9,20 @@
   *
 -->
 <template>
+  <a-card class="homepage" size="small" :bordered="false" :hoverable="true">
+    <h1 style="text-align: center">Welcome to EMA Testing website</h1>
+    <p>This website is designed to assist researchers from the
+      School of Psychological Science in conducting EMA testing and other research.
+      The website enables researchers to create accounts for their work, allowing users to edit the database through the web interface.
+      Additionally, it offers the capability to establish distinct user permissions, thereby enhancing data security.</p>
+    <p>Users can refer to the user manual for more detailed operational instructions.
+      If technicians need to view or edit the code, you can access our project and technical documentation.
+      If you want to upload and download the database, you can click the buttons below.</p>
+    <p>button</p>
+    <p>contact</p>
+
+
+  </a-card>
 <!--  <div>-->
 <!--    <input type="file" ref="fileInput" @change="onFileChange" />-->
 <!--    <button @click="uploadFile">upload</button>-->
@@ -30,12 +44,12 @@ export default {
       this.selectedFile = event.target.files[0];
     },
 
-    uploadFile() {
-      const formData = new FormData();
-      formData.append('file', this.selectedFile);
-      let fileResult = fileuploadApi.upload(formData);
-      console.log(fileResult);
-    },
+    // uploadFile() {
+    //   const formData = new FormData();
+    //   formData.append('file', this.selectedFile);
+    //   let fileResult = fileuploadApi.upload(formData);
+    //   console.log(fileResult);
+    // },
   },
 };
 </script>
