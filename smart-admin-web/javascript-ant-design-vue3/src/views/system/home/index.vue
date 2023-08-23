2da39a9fa6b68d5f788c1e37bba48d79ee4eba36
Zhenhai Qi <qizhenhai00@outlook.com>
2023-08-23 22:19:47 +0100

    update home page

--- smart-admin-web/javascript-ant-design-vue3/src/views/system/home/index.vue
+++ smart-admin-web/javascript-ant-design-vue3/src/views/system/home/index.vue
@@ -1,13 +1,4 @@
-<!--
-  * 首页
-  *
-  * @Author:    1024创新实验室-主任：卓大
-  * @Date:      2022-09-12 22:34:00
-  * @Wechat:    zhuda1024
-  * @Email:     lab1024@163.com
-  * @Copyright  1024创新实验室 （ https://1024lab.net ），Since 2012
-  *
--->
+
 <template>
   <a-card class="homepage" size="small" :bordered="false" :hoverable="true">
     <h1 style="text-align: center">Welcome to EMA Testing website</h1>
@@ -20,22 +11,19 @@
       If technicians need to view or edit the code, you can access <a href="https://github.com/Ycueh/SmartWatch" target="_blank" title="project repository">our project</a>
       and <a href="https://github.com/Ycueh/SmartWatch/blob/master/Technical%20Documentation" target="_blank" title="documentation">technical documentation</a>.
       If you want to upload and download the database, you can click the buttons below. (Note: The uploaded database format needs to be consistent with EMADATA.db)</p>
-<!--    <div>-->
-<!--      <input type="file" ref="fileInput" @change="onFileChange" />-->
-<!--      <button @click="uploadFile">Upload</button>-->
-<!--    </div>-->
-<!--    <div class="download">-->
-<!--      &lt;!&ndash;-download&ndash;&gt;-->
-<!--      <a-button type="text" @click="download" >-->
-<!--        Download-->
-<!--      </a-button>-->
-<!--    </div>-->
     <div>
       <input type="file" ref="fileInput" @change="handleFileChange" />
       <button @click="uploadFile">Upload File</button>
+    </div>
+    <div class="download-file">
       <button @click="downloadFile">Download File</button>
     </div>
-    <p>contact</p>
+    <p>If you have any questions, you can contact us through the email below.</p>
+    <p>
+      <ul style="list-style: none">
+        <li><a href="mailto:lc22175@bristol.ac.uk">lc22175@bristol.ac.uk</a></li>
+      </ul>
+    </p>
 
 
   </a-card>
@@ -66,13 +54,12 @@ export default {
       const formData = new FormData();
       formData.append("file", this.selectedFile);
 
-      // Use the upload API method from your API file
       fileTransferApi.upload(formData)
           .then(response => {
-            console.log(response.data); // Success message from the backend
+            alert(response.data);
           })
           .catch(error => {
-            console.error(error);
+            alert(error)
           });
     },
     downloadFile() {
